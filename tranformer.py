3c7214a9dbec822d48aefc242ddb73269805560e
Felipaus3465 <89425266+Felipaus3465@users.noreply.github.com>
2024-06-14 18:05:23 -0600

    conexion funcional grafo

--- tranformer.py
+++ tranformer.py
@@ -1,24 +1,31 @@
 import json
 
-
+def getTargetNode(input_data, key):
+    counter=0
+    for node in input_data['nodes']:
+        if key==node['key']:
+            return counter
+        counter+=1
+    return
 
 # Function to transform the input JSON structure to the desired format
 def transform_json(input_data):
     nodes = []
     links = []
-
+    counter=0
     # Process nodes
     for node in input_data['nodes']:
         nodes.append({
             "name": node['attributes']['label'],
             "group": 1  # Assuming a default group value as the input JSON does not specify group
         })
+        counter+=1
     
     # Process edges
     for edge in input_data['edges']:
         links.append({
-            "source": int(edge['source']),
-            "target": int(edge['target']),
+            "source": getTargetNode(input_data,edge['source']),
+            "target": getTargetNode(input_data,edge['target']),
             "value": edge['attributes']['weight']
         })
 
@@ -27,7 +34,7 @@ def transform_json(input_data):
         "nodes": nodes,
         "links": links
     }
-    
+    print(counter)
     return transformed_data
 
 # Function to read input JSON file, transform it, and write to output JSON file
